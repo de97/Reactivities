45fc7563de9249f43d07dee8cd3d4d5f2a268f4c
de97 <danielkofi@hotmail.co.ul>
2020-12-16 17:36:54 +0000

    End of section 5

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect, Fragment } from "react";
+import { Container } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import axios from "axios";
+import { IActivity } from "../models/activity";
+import NavBar from "../../features/nav/NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+    null
+  );
+
+  const [editMode, setEditMode] = useState(false);
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.filter((a) => a.id === id)[0]);
+    setEditMode(false);
+  };
+
+  const handleOpenCreateForm = () => {
+    setSelectedActivity(null);
+    setEditMode(true);
+  };
+
+  //set activities spread operator activities into a new array
+  const handleCreateActivity = (activity: IActivity) => {
+    setActivities([...activities, activity]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  };
+  //contain a new array of all the activities that do not match the id of the activity we are passing in
+  const handleEditActivity = (activity: IActivity) => {
+    setActivities([
+      ...activities.filter((a) => a.id !== activity.id),
+      activity,
+    ]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  };
+
+  const handleDeleteActivity = (id: string) => {
+    //return all activities that do not match the id we are passing in as a string
+    setActivities([...activities.filter((a) => a.id !== id)]);
+  };
+
+  //get the activities and set the state of activities
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        let activities: IActivity[] = [];
+        response.data.forEach((activity) => {
+          activity.date = activity.date.split(".")[0];
+          activities.push(activity);
+        });
+        setActivities(activities);
+      });
+  }, []);
+  //[] empty array stops method from running more then once
+
+  return (
+    <Fragment>
+      <NavBar openCreateForm={handleOpenCreateForm} />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          selectedActivity={selectedActivity}
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
+          deleteActivity={handleDeleteActivity}
+        />
+      </Container>
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ client-app/src/app/models/activity.ts
@@ -0,0 +1,10 @@
+//you should start interface with an I, use interface when defining structure of object and strong typing 
+export interface IActivity {
+    id: string;
+    title: string;
+    description: string;
+    category: string;
+    date: string;
+    city: string;
+    venue: string;
+}
